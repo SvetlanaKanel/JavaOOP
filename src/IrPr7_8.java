9394f599b1ce69b21c8d58b0d26013f2095a0ab1
kanel <branles@yandex.ru>
2021-12-13 21:37:48 +0300

    Code clinned

--- src/IrPr7_8.java
+++ src/IrPr7_8.java
@@ -4,7 +4,6 @@ public class IrPr7_8 {
         System.out.println("______________________________ex " + number + "_______________________________");
     }
 
-    // метод для теста сравнивает int expected result с actual result
     public static String assertIntegerResults(int actualNumber, int expectedNumber) {
         if (actualNumber == expectedNumber) {
 
@@ -13,8 +12,6 @@ public class IrPr7_8 {
         return "Failed";
     }
 
-
-    // метод для теста сравнивает String expected result с actual result
     public static String assertStringResults(String actualResult, String expectedResult) {
         if (actualResult.equals(expectedResult)) {
 
@@ -23,7 +20,6 @@ public class IrPr7_8 {
         return "Failed";
     }
 
-    // метод для теста сравнивает boolean expected result с actual result
     public static String assertBooleanResults(boolean actualResult, boolean expectedResult) {
         if (actualResult == expectedResult) {
 
@@ -33,48 +29,35 @@ public class IrPr7_8 {
         return "Failed";
     }
 
-    // метод печати результата теста c boolean
     public static void printBooleanTest(boolean actualResult, boolean expectedResult) {
         System.out.println("Test result\t\t\t" + assertBooleanResults(actualResult, expectedResult));
     }
 
-    //1
-
     public static boolean areEqual(int a1, int a2, int b1, int b2) {
-//        if ((a1 == a2) && (b1 == b2)) {
-//
-//            return true;
-//        }
-//        return false;
 
         return ((a1 == a2) && (b1 == b2));
     }
 
-    //2
     public static boolean isNotTrue(int a, int b) {
 
         return (!(a < b));
     }
 
-    //3
     public static boolean areEqualFourTrees(String tree1, String tree2, String tree3, String tree4) {
 
         return ((tree1.equals(tree2)) || (tree3.equals(tree4)));
     }
 
-    //4
     public static boolean areNotEqualTwoTrees(String tree1, String tree2) {
 
         return !tree1.equals(tree2);
     }
 
-    //5
     public static boolean areNotFalseResults(int a, int b, int c, int d) {
 
         return !(a < b) && (c > d);
     }
 
-    //8
     public static void ageForDriving(int age) {
         int legalAge = 16;
         if (age >= legalAge) {
@@ -83,18 +66,6 @@ public class IrPr7_8 {
             System.out.println("You can not get a driver's license");
         }
     }
-
-
-
-
-
-
-
-    /* ex 1
-     * Напишите алгоритм проверки числа на четность. Если число четное, удвойте это число.
-     * Напишите тест, который валидирует (проверяет правильность работы) ваш код.
-     * Выведите результат проверки чисел 2, 5, 14, 67, 0 на печать.
-     */
 
     public static boolean isNumberEven(int number) {
         if (number % 2 == 0) {
@@ -129,13 +100,6 @@ public class IrPr7_8 {
         }
 
     }
-    /*
-     * Напишите алгоритм проверки возраста на соответствие условиям:
-     * Голосовать можно с 18 лет
-     * Машину можно водить с 16 лет
-     * В школу можно идти с 5 лет
-     * Напишите тест, который валидирует ваш код. Придумайте тестовые данные. Выведите результат проверки на печать.
-     */
 
     public static String isAgeForVoting(int age) {
         if (age >= 18) {
@@ -158,15 +122,6 @@ public class IrPr7_8 {
         return "You cannot go to school.";
     }
 
-    /*
-     * Напишите алгоритм программы, которая проверяет оценку ученика и выполняет следующие действия:
-     * 5 - выдать похвальную грамоту и перевести в следующий класс
-     * 4 - перевести в следующий класс
-     * 3 - дать задание на лето и перевести в следующий класс
-     * 2 - вызвать родителей и оставить в текущем классе на второй год
-     * Напишите тест, который валидирует ваш код.
-     * Выведите результат проверки на печать.
-     */
     public static String gradeRemark(int grade) {
         if (grade == 5) {
             return "Выдать похвальную грамоту и перевести в следующий класс.";
@@ -179,13 +134,6 @@ public class IrPr7_8 {
         } else
             return "Ошибка ввода данных.";
     }
-
-    /*
-     * Напишите алгоритм программы, которая проверяет число типа int на количество разрядов, и выводит результат проверки.
-     *( Например, “It’s a two-digit number.”, “It’s a five-digit number.”, etc)
-     * Напишите тест, который валидирует ваш код.
-     * Выведите результат проверки на печать.
-     */
 
     public static String countNumberOfDigits(int number) {
         if (number >= -9 && number <= 9) {
@@ -210,12 +158,6 @@ public class IrPr7_8 {
         return "Invalid input. Please, enter Integer";
     }
 
-    //5
-    /*
-     * Напишите алгоритм программы, которая складывает числа, которые делятся на 3 без остатка,
-     * и вычитает числа, которые делятся на 5 без остатка.
-     */
-
     public static int getThreeOrFiveDividing(int number) {
         int result = 0;
         if (number % 3 == 0) {
@@ -226,8 +168,6 @@ public class IrPr7_8 {
         }
         return result;
     }
-
-    //************************************************************************************************************
 
     public static void main(String[] args) {
         printLine(1);
@@ -244,14 +184,13 @@ public class IrPr7_8 {
 
         printLine(3);
 
-        // ("Сосна" = "Дуб") ИЛИ ("Вишня" = "Клён");
         String s = "Сосна";
         String d = "Дуб";
         String v = "Вишня";
         String k = "Клён";
 
         System.out.println(areEqualFourTrees(s, d, v, k));
-        //Test
+
         boolean actualResult = areEqualFourTrees(s, d, v, k);
         boolean expectedResult = false;
         System.out.println(assertBooleanResults(actualResult, expectedResult));
@@ -273,10 +212,9 @@ public class IrPr7_8 {
         printBooleanTest(actualResult, expectedResult);
 
         printLine(8);
-//“Водительские права можно получить, только когда исполнится 16 лет.”
+
         int age = 17;
         ageForDriving(age);
-
 
         printLine(1);
 
@@ -286,17 +224,9 @@ public class IrPr7_8 {
         System.out.println("Even or odd\t\t\t" + evenOrOddNumber(j1) + "\n"
                 + "Action\t\t\t\t" + getAction(j1) + "\n" + "New number\t\t\t" + checkNumber(j1));
 
-        //TEST
         System.out.println("Test result\t\t\t" + assertIntegerResults(checkNumber(j1), 28));
 
         printLine(2);
-        /*
-         * Напишите алгоритм проверки возраста на соответствие условиям:
-         * Голосовать можно с 18 лет
-         * Машину можно водить с 16 лет
-         * В школу можно идти с 5 лет
-         * Напишите тест, который валидирует ваш код. Придумайте тестовые данные. Выведите результат проверки на печать.
-         */
 
         age = 2;
         System.out.println("Age\t\t\t" + age);
@@ -308,16 +238,6 @@ public class IrPr7_8 {
 
         printLine(3);
 
-        /*
-         * Напишите алгоритм программы, которая проверяет оценку ученика и выполняет следующие действия:
-         * 5 - выдать похвальную грамоту и перевести в следующий класс
-         * 4 - перевести в следующий класс
-         * 3 - дать задание на лето и перевести в следующий класс
-         * 2 - вызвать родителей и оставить в текущем классе на второй год
-         * Напишите тест, который валидирует ваш код.
-         * Выведите результат проверки на печать.
-         */
-
         int grade = 5;
         System.out.println("Оценка\t\t\t" + grade);
         System.out.println("Сообщение:\t\t" + gradeRemark(grade));
@@ -327,23 +247,12 @@ public class IrPr7_8 {
         );
 
         printLine(4);
-        /*
-         * Напишите алгоритм программы, которая проверяет число типа int на количество разрядов, и выводит результат проверки.
-         *( Например, “It’s a two-digit number.”, “It’s a five-digit number.”, etc)
-         * Напишите тест, который валидирует ваш код.
-         * Выведите результат проверки на печать.
-         */
 
         int number7 = 1654;
         System.out.println(number7);
         System.out.println(countNumberOfDigits(number7));
 
         printLine(5);
-
-        /*
-         * Напишите алгоритм программы, которая складывает числа, которые делятся на 3 без остатка,
-         * и вычитает числа, которые делятся на 5 без остатка.
-         */
 
         int number = 10;
         int expectedResult2 = -10;
@@ -354,6 +263,5 @@ public class IrPr7_8 {
                 + "Expected result\t\t" + expectedResult2 + "\n"
                 + "Actual result\t\t" + actualResult2 + "\n"
                 +assertIntegerResults(actualResult2, expectedResult2));
-
     }
 }
